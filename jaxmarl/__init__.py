e2b4728f3e736cf711c03a05af9af8fcff42e06c
Asim Osman <a.osman@instadeep.com>
2025-12-10 19:41:38 +0000

    version

--- jaxmarl/__init__.py
+++ jaxmarl/__init__.py
@@ -1,4 +1,4 @@
-# from .registration import make, registered_envs
+from .registration import make, registered_envs
 
-# __all__ = ["make", "registered_envs"]
-# __version__ = "0.0.5"
+__all__ = ["make", "registered_envs"]
+__version__ = "0.0.5"
